7f24d41b6f3d52c60ddb2f21549d544c9f60bd95
Gael Garcia <gcarlogael875@gmail.com>
2026-06-08 14:09:56 -0600

    Preparar backend para Render

--- backend/src/app.js
+++ backend/src/app.js
@@ -39,6 +39,8 @@ app.get('/', (req, res) => {
     res.send('Servidor funcionando');
 });
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log('Servidor en puerto 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor en puerto ${PORT}`);
 });
